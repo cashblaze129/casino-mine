f8ddba3bfcbbd82f5748f49c95c3e5b2875a1412
cashblaze127 <cashblaze129@gmail.com>
2023-06-21 12:40:25 -0700

    feat: add max or min bet amount setting

--- src/config/global.const.ts
+++ src/config/global.const.ts
@@ -4,5 +4,7 @@ export const config = {
   appKey: process.env.REACT_APP_APP_TOKEN_KEY,
   tokenString:
     JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.token ||
-    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.token
+    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.token,
+  minBetAmount: 10,
+  maxBetAmout: 1000
 };
